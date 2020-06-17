e8dca69a56392239699aaad9cef7c4f56b307a18
Boyuan Yu <boyuan.yu@mail.mcgill.ca>
2020-06-17 09:46:34 -0400

    added ParaView sampling file

---
+++ sampling.py
@@ -0,0 +1,31 @@
+try: paraview.simple
+except: from paraview.simple import *
+paraview.simple._DisableFirstRenderCameraReset()
+
+q = 4
+U = 1
+xx = q/U*4*6
+yy = q/U*4
+
+my_foam = FindSource("case.OpenFOAM")
+SetActiveSource(my_foam)
+
+tsteps = my_foam.TimestepValues
+PlotOverLine1 = PlotOverLine( Source="High Resolution Line Source" )
+DataRepresentation7 = Show()
+
+PlotOverLine1.Source.Point1 = [xx/2, 0.0, 0.1]
+PlotOverLine1.Source.Point2 = [xx/2, yy, 0.1]
+
+source = PlotOverLine1
+
+#for TimeStepNum in range(0,len(tsteps)):
+for TimeStepNum in range(0,100,20):
+    view = GetActiveView()
+    view.ViewTime = tsteps[TimeStepNum]
+    Render()
+    writer = CreateWriter("%dvel.csv" %(TimeStepNum), source)
+    writer.FieldAssociation = "Points"
+    writer.UpdatePipeline()
+    Render()
+    del writer
